049f2ff0d17b3a55369f5b5227225ce2edecdaab
Nidhin <nidhin@mobomo.com>
2016-08-08 14:37:58 +0530

    side bar added

--- app/assets/javascripts/apidoco/custom.js
+++ app/assets/javascripts/apidoco/custom.js
@@ -2,3 +2,23 @@ var app = angular.module('apiDocoApp', ['ngPrettyJson']);
 app.controller('v1_json', function($scope) {
 
 });
+app.controller('v1_json_sidebar', function($scope) {
+
+});
+
+// app.filter('lower_case', function() {
+//   return function(input, optional1, optional2) {
+//     var output;
+//     output = input.toLowerCase().split(" ").join("_");
+//     return output;
+//   }
+// });
+
+$(document).ready(function () {
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+
+
+});
