cabdae5334cd15785f996aab9c6d88bc21cd45b3
Al Ameen <k.ameen88@gmail.com>
2016-08-21 12:44:56 +0530

    - adding du-scroll directive - updating DocumentationsController

--- app/assets/javascripts/apidoco/custom.js
+++ app/assets/javascripts/apidoco/custom.js
@@ -1,24 +1,15 @@
-var app = angular.module('apiDocoApp', ['ngPrettyJson']);
-app.controller('v1_json', function($scope) {
+angular.module('apiDocoApp', ['ngPrettyJson', 'duScroll'])
+.value('duScrollOffset', 100) // offset from top to the div
+.value('duScrollBottomSpy', true) // To make the last du-scrollspy link active when scroll reaches page/container bottom
+.controller('DocumentationController', ['$scope', function ($scope) {
+  $scope.init = function () {
+    $(document).ready(function () {
+      $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+      });
+    });
+  };
 
-});
-app.controller('v1_json_sidebar', function($scope) {
-
-});
-
-// app.filter('lower_case', function() {
-//   return function(input, optional1, optional2) {
-//     var output;
-//     output = input.toLowerCase().split(" ").join("_");
-//     return output;
-//   }
-// });
-
-$(document).ready(function () {
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-  });
-
-
-});
+  $scope.init();
+}]);
