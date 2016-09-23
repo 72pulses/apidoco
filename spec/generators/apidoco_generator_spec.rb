1b58235818981be8d44822f2ae49d218329c9456
Al Ameen <k.ameen88@gmail.com>
2016-09-23 12:37:46 +0530

    refactoring generator spec

--- spec/generators/apidoco_generator_spec.rb
+++ spec/generators/apidoco_generator_spec.rb
@@ -1,6 +1,6 @@
-require "rails/generators"
-require "generators/apidoco_generator"
-require "generator_spec"
+require 'rails/generators'
+require 'generators/apidoco_generator'
+require 'generator_spec'
 
 describe ApidocoGenerator, type: :generator do
   def remove_docs_directory
@@ -12,20 +12,20 @@ describe ApidocoGenerator, type: :generator do
   end
 
   context 'default actions' do
-    it "creates a crud documentation files" do
+    it 'creates a crud documentation files' do
       run_generator %w(api/v1/posts)
-      assert_file "#{Rails.root}/docs/api/v1/posts/create.json", /api\/v1\/posts.json/
-      assert_file "#{Rails.root}/docs/api/v1/posts/destroy.json", /api\/v1\/posts\/:id.json/
-      assert_file "#{Rails.root}/docs/api/v1/posts/index.json", /api\/v1\/posts.json/
-      assert_file "#{Rails.root}/docs/api/v1/posts/show.json", /api\/v1\/posts\/:id.json/
-      assert_file "#{Rails.root}/docs/api/v1/posts/update.json", /api\/v1\/posts\/:id.json/
+      assert_file "#{Rails.root}/docs/api/v1/posts/create.json", %r{api/v1/posts.json}
+      assert_file "#{Rails.root}/docs/api/v1/posts/destroy.json", %r{api/v1/posts/:id.json}
+      assert_file "#{Rails.root}/docs/api/v1/posts/index.json", %r{api/v1/posts.json}
+      assert_file "#{Rails.root}/docs/api/v1/posts/show.json", %r{api/v1/posts/:id.json}
+      assert_file "#{Rails.root}/docs/api/v1/posts/update.json", %r{api/v1/posts/:id.json}
     end
   end
 
   context 'custom actions' do
     let(:upload_acton) { 'upload' }
 
-    it "creates the documentation files for the actions supplied in arguments" do
+    it 'creates the documentation files for the actions supplied in arguments' do
       run_generator ['api/v1/posts', upload_acton]
 
       assert_file "#{Rails.root}/docs/api/v1/posts/#{upload_acton}.json"
