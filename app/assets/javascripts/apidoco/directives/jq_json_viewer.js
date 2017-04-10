5192a5ccdb0ff377cd2425835c0d279b44cc3bfa
Al Ameen <alameenkhader@gmail.com>
2017-04-10 15:55:24 +0530

    Feature/collapsible json (#27)
    * Json Formatter
    
    * removing application record
    
    * adding collapsible json viewer
    
    * headers, params, notes, request and responses - hiding keys if they are missing in json.

---
+++ app/assets/javascripts/apidoco/directives/jq_json_viewer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('apiDocoApp').directive('jqJsonViewer', [function() {
+  return {
+    restrict: 'A',
+    scope: {
+      data: "="
+    },
+    link: function(scope, elem, attrs) {
+      $(elem).jsonViewer(scope.data);
+    }
+  };
+}]);
--- app/models/apidoco/application_record.rb
+++
@@ -1,5 +0,0 @@
-module Apidoco
-  class ApplicationRecord < ActiveRecord::Base
-    self.abstract_class = true
-  end
-end
