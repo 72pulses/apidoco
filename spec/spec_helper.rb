aa5ab4894a0e4bc06b557990e2095c0cf88ddf68
Al Ameen <alameenkhader@gmail.com>
2017-06-30 12:26:17 +0530

    updating gems, adding schema, removing depricated rspec autorun

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 Rails.backtrace_cleaner.remove_silencers!
 
