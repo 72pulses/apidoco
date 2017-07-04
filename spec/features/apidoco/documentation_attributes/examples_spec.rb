155ec64e9ae2c70ae3e0f369c105dd4c2a7b5d81
Al Ameen <alameenkhader@gmail.com>
2017-07-04 15:09:01 +0530

    adding more test coverage

---
+++ spec/features/apidoco/documentation_attributes/examples_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature 'Document Attributes', type: :feature do
+  describe 'examples', js: true do
+    it 'will list the documentation' do
+      create_json = {
+        published: true,
+        examples: [{
+          request: {
+            currency: {
+              name: "US dollar",
+              symbol: "$",
+              exchange_rate: "1.3"
+            }
+          },
+          response: {
+            currency: {
+              id: "8",
+              name: "us dollar",
+              symbol: "$",
+              exchange_rate: "1.3"
+            }
+          }
+        }]
+      }
+      DocumentationBuilder.create(create_json)
+
+      visit '/apidoco'
+      request = create_json[:examples][0][:request][:currency]
+      response = create_json[:examples][0][:response][:currency]
+
+      request.each do |key, value|
+        expect(page).to have_text(format('"%s": "%s"', key, value))
+      end
+      response.each do |key, value|
+        expect(page).to have_text(format('"%s": "%s"', key, value))
+      end
+    end
+  end
+end
