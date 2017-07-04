7051889846c66d84f3a342b991da6d5cafdb03c0
Al Ameen <alameenkhader@gmail.com>
2017-07-04 18:00:18 +0530

    # Adding folder parser
    Conflicts:
    	lib/apidoco.rb

--- lib/apidoco.rb
+++ lib/apidoco.rb
@@ -4,6 +4,7 @@ require 'apidoco/engine'
 require 'apidoco/version_parser'
 require 'apidoco/version_documentation'
 require 'apidoco/resource_documentation'
+require 'apidoco/folder_parser'
 
 module Apidoco
   mattr_accessor :auth_name, :auth_password
