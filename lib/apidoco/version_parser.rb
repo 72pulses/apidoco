a21e74d031c1ba0a3394159bcdaecd54fb5f1c7a
Steve Lewis <steve.l@lojistic.com>
2020-05-19 08:11:44 -0700

    Fix forced reference to `api` in version parser path.

--- lib/apidoco/version_parser.rb
+++ lib/apidoco/version_parser.rb
@@ -4,7 +4,7 @@ module Apidoco
     attr_accessor :base_path
 
     def initialize
-      self.base_path = Rails.root.join('docs', 'api')
+      self.base_path = Rails.root.join('docs')
     end
 
     def documentations
