a47a6f7d03f2c3ff4d8335743a9e653120229905
alameenkhader <alameenkhader@gmail.com>
2020-08-20 23:25:23 -0700

    adding package.json

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,4 @@
-Rails.application.config.assets.precompile += %w[apidoco_manifest.js]
+
+# Rails.application.config.assets.paths <<
+
+# Rails.application.config.assets.precompile += %w[apidoco_manifest.js]
--- lib/apidoco/engine.rb
+++ lib/apidoco/engine.rb
@@ -1,5 +1,7 @@
 module Apidoco
   class Engine < ::Rails::Engine
     isolate_namespace Apidoco
+
+    config.assets.paths <<  Pathname.new(File.expand_path('../../../node_modules', __FILE__))
   end
 end
